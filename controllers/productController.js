7176bd85f2ef53be295d0306279a3b660514fc13
TedoDonchev <37153281+TedoDonchev@users.noreply.github.com>
2021-02-13 16:35:39 +0200

    added database

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,28 @@
 const { Router } = require('express');
 const router = Router();
+const prodcutService = require('../services/productService');
+
+router.get('/', (req, res) => {
+    res.render('home', {layout: 'main', title: 'Browse'});  
+});
+
 
 router.get('/create', (req, res) => {
     res.render('create', {layout: 'main', title: 'Create'});  
 });
 
-router.get('/', (req, res) => {
-    res.render('home', {layout: 'main', title: 'Browse'});  
+router.post('/create', (req, res) => {
+    // TODO: Validate input
+
+    prodcutService.create(req.body);
+
+    res.redirect('/');  
+});
+
+
+router.get('/details/:productId', (req, res) => {
+    console.log(req.params.productId);
+    res.render('details', {layout: 'main', title: 'Details'});  
 });
 
 module.exports = router;
---
+++ services/productService.js
@@ -0,0 +1,32 @@
+const Cube = require('../models/Cube');
+const uniqid = require('uniqid');
+
+const fs = require('fs');
+const productsData = require('../config/products.json');
+
+function create(data) {
+    
+    let cube = new Cube(
+        uniqid(), 
+        data.name, 
+        data.description, 
+        data.imageUrl, 
+        data.difficultyLevel
+    );
+
+    productsData.push(cube);
+    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData), (err) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+    })
+}
+
+function getAll() {
+    return productsData;
+}
+
+module.exports = {
+    create
+}
